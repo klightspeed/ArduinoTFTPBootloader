304599138f6ecd82a620ec2d0361632803c3fdbf
Ben Peddell <klightspeed@killerwolves.net>
2015-05-17 20:47:11 +1000

    Improve flash writer

--- bootsafe/flash.c
+++ bootsafe/flash.c
@@ -7,10 +7,12 @@ extern void *__vectors;
 
 void ___flash_write_page(void *pageaddr, void *src, uint16_t valid1, uint16_t valid2) {
     if ((valid1 ^ (uint16_t)pageaddr) == 0xc0de && (valid2 ^ (uint16_t)src) == 0xcafe && (uint16_t)pageaddr < (uint16_t)(&__vectors)) {
+        pageaddr = (void *)((uint16_t)pageaddr & -SPM_PAGESIZE);
+
         if (compare_const_zx(src, pageaddr, SPM_PAGESIZE)) {
             ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
-                register uint16_t __pageaddr asm("r30") = (uint16_t)pageaddr & -SPM_PAGESIZE;
-                register void *__src asm("r26") = src;
+                register uint16_t __pageaddr asm("r30") = (uint16_t)pageaddr;
+                register void *__src asm("r28") = src;
 
                 wdt_reset();
                 boot_spm_busy_wait();
@@ -21,20 +23,20 @@ void ___flash_write_page(void *pageaddr, void *src, uint16_t valid1, uint16_t va
 
                 do {
                     asm volatile (
-                        "ld r0, X+\n\t"
-                        "ld r1, X+\n\t"
+                        "ldd r0, Y+\n\t"
+                        "ldd r1, Y+\n\t"
                         "sts %[spmreg], %[pagefill]\n\t"
                         "spm\n\t"
                         "clr r1\n\t"
                         "adiw r30, 2\n\t"
-                        : "+x" (__src), "+z" (__pageaddr)
+                        : "+y" (__src), "+z" (__pageaddr)
                         : [spmreg] "i" (_SFR_MEM_ADDR(__SPM_REG)),
                           [pagefill] "r" ((uint8_t)__BOOT_PAGE_FILL)
                         : "r0"
                     );
                 } while (LO8(__pageaddr) & (uint8_t)(~(-(SPM_PAGESIZE))));
 
-                __pageaddr -= SPM_PAGESIZE;
+                __pageaddr = (uint16_t)pageaddr;
 
                 boot_page_write(__pageaddr);
                 boot_spm_busy_wait();
